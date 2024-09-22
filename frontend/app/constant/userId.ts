8773cb7556ae3b2567c6399f7f1f5e6eb48bf79d
Yasser MESTAOUI <128167310+yas0se@users.noreply.github.com>
2024-09-22 23:07:54 +0100

    user interface

--- frontend/app/constant/userId.ts
+++ frontend/app/constant/userId.ts
@@ -1,12 +1,22 @@
 import jwt from 'jsonwebtoken';
 
-let user: any = null; // Initialize user variable
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  isAdmine: boolean;
+  email: string;
+  password: string;
+  phone: string;
+}
+
+let user: User | null = null; // Initialize user variable
 
 const token = localStorage.getItem("token");
 
 if (token) {
   try {
-    const payload = jwt.verify(token, "user_key") as { user: any };
+    const payload = jwt.verify(token, "user_key") as { user: User  };
     user = payload.user; // Assign the user from payload
     console.log(payload);
   } catch (err) {
